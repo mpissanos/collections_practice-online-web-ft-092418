094e6705bedeba8afff2e7e87e46dda08117c0ad
Manny Pissanos <mpissano@gmail.com>
2018-09-24 18:05:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,7 @@ def sum_array(arr)
 end
 
 def add_s(arr)
- arr.each_with_index.collect{|element, index| if index != 1  element + "s"}
+ arr.each_with_index.collect{|element, index| element + "s"}
 end
 
 
